0c489f2e232e33cd27c7abe725faaa41f14bc7e2
chenyadong <chen17638937431@163.com>
2022-09-29 00:19:07 +0800

    规范化发过

--- tr-user/src/main/java/com/cdd/test/controller/UserController.java
+++ tr-user/src/main/java/com/cdd/test/controller/UserController.java
@@ -4,10 +4,7 @@ import com.cdd.test.pojo.User;
 import com.cdd.test.service.UserService;
 import com.cdd.test.utils.R;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author chenyadong
@@ -27,6 +24,11 @@ public class UserController {
     @PostMapping("/list")
     public R list(){
         return userService.list();
+    }
+
+    @GetMapping("/test")
+    public void test(){
+        System.out.println("aaaaa");
     }
 
 }
